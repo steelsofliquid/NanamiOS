d15a4db649ac9d2cd986ff404a40a5c9aea874d6
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2023-10-27 16:32:55 -0400

    Added content for version 1.00.3 (part of the "Reimu" codename project)

---
+++ version1.0/1.00.3/kernel.cpp
@@ -0,0 +1,58 @@
+
+#include "types.h"
+#include "gdt.h"
+
+
+void printf(char* str)
+{
+  static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+  
+  static uint8_t x =0 , y=0;
+
+  for(int i = 0; str[i] != '\0'; ++i)
+  {
+
+    switch(str[i])
+    {
+
+      case '\n':
+        y++;
+        x = 0;
+        break;
+
+      default:
+        VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+        x++;
+        break;
+    }
+
+    if(x >= 80)
+    {
+      y++;
+      x = 0;
+    }
+
+    if(y >= 25)
+    {
+      for(y = 0; y < 25; y++)
+        for(x = 0; x < 80; x++)
+          VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+
+      x = 0;
+      y=0;
+    }
+  }
+}
+
+
+
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
+{
+  printf("----- NanamiOS 1.00.3 [Codename \"Reimu\"] -----\n --- By SteelsOfLiquid - https://www.youtube.com/@steelsofliquid]\n\n[Caution!] Use this build at your risk. I haven't developed drivers or a command line yet, so it basically does nothing. Sorry! >_<");
+  
+  GlobalDescriptorTable gdt;
+
+  while(1);
+  
+}
+

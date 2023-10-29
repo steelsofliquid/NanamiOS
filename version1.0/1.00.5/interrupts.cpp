fb724ebe5b3b2f9db957bb865fbbaebecc148186
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2023-10-28 21:24:02 -0400

    Added 1.00.5 content

---
+++ version1.0/1.00.5/interrupts.cpp
@@ -0,0 +1,82 @@
+
+#include "interrupts.h" // This OS' source code is so dependent on its files, it'd be dependency hell if I didn't provide all of the OS' code! ^_^
+
+void printf(char* str);
+
+
+InterruptManager::GateDescriptor InterruptManager::interruptDescriptorTable[256];
+
+
+void InterruptManager::SetInterruptDescriptorTableEntry(
+    uint8_t interruptNumber,
+    uint16_t codeSegmentSelectorOffset,
+    void (*handler)(),
+    uint8_t DescriptorPrivilegeLevel,
+    uint8_t DescriptorType
+)
+{
+
+    const uint8_t IDT_DESC_PRESENT = 0x80;
+
+    interruptDescriptorTable[interruptNumber].handlerAddressLowBits = ((uint32_t)handler) & 0xFFFF;
+    interruptDescriptorTable[interruptNumber].handlerAddressHighBits = (((uint32_t)handler) >> 16) & 0xFFFF;
+    interruptDescriptorTable[interruptNumber].gdt_codeSegmentSelector = codeSegmentSelectorOffset;
+    interruptDescriptorTable[interruptNumber].access = IDT_DESC_PRESENT | DescriptorType | ((DescriptorPrivilegeLevel&3) << 5);
+    interruptDescriptorTable[interruptNumber].reserved = 0;
+
+}
+
+InterruptManager::InterruptManager(GlobalDescriptorTable* gdt)
+: picMasterCommand(0x20), // Placed a semicolon here, but realized it's supposed to be a normal colon. Silly me! | .|
+  picMasterData(0x21), // placed colons separately here. could tell before the guy in the tutorial because vs code (what i'm using to code the entire os) colored the first one of these four different from the rest
+  picSlaveCommand(0xA0),
+  picSlaveData(0xA1) // This doesn't get a colon. That created an error >_<
+{
+    uint16_t CodeSegment = gdt->CodeSegmentSelector();
+    const uint8_t IDT_INTERRUPT_GATE = 0xE;
+
+    for(uint16_t i = 0; i < 256; i++)
+        SetInterruptDescriptorTableEntry(i, CodeSegment, &IgnoreInterruptRequest, 0, IDT_INTERRUPT_GATE);
+
+    SetInterruptDescriptorTableEntry(0x20, CodeSegment, &HandleInterruptRequest0x00, 0, IDT_INTERRUPT_GATE);
+    SetInterruptDescriptorTableEntry(0x21, CodeSegment, &HandleInterruptRequest0x01, 0, IDT_INTERRUPT_GATE);
+
+    picMasterCommand.Write(0x11);
+    picSlaveCommand.Write(0x11);
+
+    picMasterData.Write(0x20);
+    picSlaveData.Write(0x28);
+
+    picMasterData.Write(0x04);
+    picSlaveData.Write(0x02);
+
+    picMasterData.Write(0x01);
+    picSlaveData.Write(0x01);
+
+    picMasterData.Write(0x00);
+    picSlaveData.Write(0x00);
+
+    interruptDescriptorTablePointer idt;
+    idt.size = 256 * sizeof(GateDescriptor) - 1;
+    idt.base = (uint32_t)interruptDescriptorTable;
+    asm volatile("lidt %0" : : "m" (idt));
+
+}
+
+InterruptManager::~InterruptManager() // Made a goofy mistake over and over by leaving a semicolon here. Interrupts are hard >_<
+{
+    //
+}
+
+void InterruptManager::Activate()
+{
+    asm("sti");
+}
+
+
+uint32_t InterruptManager::handleInterrupt(uint8_t interruptNumber, uint32_t esp)
+{
+    printf("\n[Interrupt] NanamiOS has been interrupted."); // Linux style! XD
+    return esp;
+
+}
